6d65722b8f195b5898682a79edfb0859640728cf
Herman De Beukelaer <herman.debeukelaer@ugent.be>
2015-08-10 22:33:35 +0200

    prepare rJAva

--- sim/java.R
+++ sim/java.R
@@ -1,6 +1,38 @@
 library(rJava)
 .jinit()
 
-java.version <- function(){
+###############################################################
+# MAXIMIZE WEIGHTED INDEX: AVERAGE BREEDING VALUE + DIVERSITY #
+###############################################################
+
+j.max.index <- function(n, values, markers, div.weight, div.measure){
+  # ...
+}
+
+######################
+# DIVERSITY MEASURES #
+######################
+
+j.MR.ENE <- function(){
+  j.new("obj.EntryToNearestEntryDistance")
+}
+
+j.HE <- function(){
+  j.new("obj.ExpectedProportionOfHeterozygousLoci")
+}
+
+#############
+# UTILITIES #
+#############
+
+j.version <- function(){
   print(.jcall("java/lang/System", "S", "getProperty", "java.runtime.version"))
+}
+
+j.load.jar <- function(){
+  .jaddClassPath("java/gs-simulation/bin/gs-simulation.jar")
+}
+
+j.new <- function(class){
+  .jnew(sprintf("org.jamesframework.gs.simulation.%s", class))
 }
