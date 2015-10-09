feb1ff2d940020fd791b092471c3796235ae1fda
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2015-10-09 09:37:09 +0200

    fixed libraries

--- init.R
+++ init.R
@@ -1,13 +1,13 @@
 # load required libraries
-library(BGLR)     # version used: 1.0.4
-library(coda)     # version used: 0.17-1
-library(rrBLUP)   # version used: 4.3
-library(synbreed) # version used: 0.10-5
-library(hypred)   # version used: 0.5
-library(gdata)    # version used: 2.17.0
-library(Hmisc)    # version used: 3.16-0
-library(rJava)    # version used: 0.10-5
-library(setRNG)   # version used: 2013.9-1
+library(BGLR)      # version used: 1.0.4
+library(coda)      # version used: 0.17-1
+library(rrBLUP)    # version used: 4.3
+library(synbreed)  # version used: 0.10-5
+library(hypred)    # version used: 0.5
+library(gdata)     # version used: 2.17.0
+library(Hmisc)     # version used: 3.16-0
+library(rJava)     # version used: 0.9-7
+library(animation) # version used: 2.4
 
 # set path to BEAGLE used by synbreed
 beaglePath <- "beaglejar"
