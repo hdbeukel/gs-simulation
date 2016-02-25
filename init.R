fd9fef621926d5b4bfd0f6bbc67257802a38e07a
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-02-25 09:55:15 +0100

    update package versions

--- init.R
+++ init.R
@@ -5,8 +5,8 @@ library(rrBLUP)    # version used: 4.3
 library(synbreed)  # version used: 0.10-5
 library(hypred)    # version used: 0.5
 library(gdata)     # version used: 2.17.0
-library(Hmisc)     # version used: 3.16-0
-library(rJava)     # version used: 0.9-7
+library(Hmisc)     # version used: 3.17-2
+library(rJava)     # version used: 0.9-8
 library(setRNG)    # version used: 2013.9-1
 library(animation) # version used: 2.4
 
