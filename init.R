435d824674fb86b942e0a3d30362d9918c9f49ef
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-10-24 14:04:43 +0200

    no error when synbreed is not available

--- init.R
+++ init.R
@@ -2,13 +2,13 @@
 library(BGLR)        # version used: 1.0.4
 library(coda)        # version used: 0.17-1
 library(rrBLUP)      # version used: 4.3
-library(synbreed)    # version used: 0.10-5
 library(hypred)      # version used: 0.5
 library(gdata)       # version used: 2.17.0
 library(Hmisc)       # version used: 3.17-2
 library(rJava)       # version used: 0.9-8
 library(setRNG)      # version used: 2013.9-1
 library(animation)   # version used: 2.4
+require(synbreed)    # version used: 0.10-5 (only for data preprocessing)
 
 # set path to BEAGLE used by synbreed
 beaglePath <- "beaglejar"
