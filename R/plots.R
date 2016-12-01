37bd3de03216ad789a0240f322d2526e996e8d5c
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-12-01 10:40:38 +0100

    attempt to fix MDS plot legend

--- R/plots.R
+++ R/plots.R
@@ -2108,8 +2108,8 @@ plot.MDS.populations <- function(type = c("markers", "qtl"), simulations, genera
       legend = method.names,
       pch = 24,
       col = sel.col,
-      pt.bg = sel.col,
-      text.width = max(sapply(method.names, strwidth))
+      pt.bg = sel.col#,
+      #text.width = max(sapply(method.names, strwidth))*1.1
     )
     
     # gain progress bars
