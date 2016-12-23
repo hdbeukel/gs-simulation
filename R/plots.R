7ac3219daff7e525c8da05ef7cf7cc656e3c6e3a
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-12-23 13:38:13 +0100

    plot function for RS vs OC

--- R/plots.R
+++ R/plots.R
@@ -126,13 +126,13 @@ get.plot.functions <- function(){
     # list(f = plot.mean.QTL.marker.LD, name = "LD", title = "Mean polymorphic QTL - marker LD",
     #       legend = "bottomleft", ylim = c(0.3, 0.9)),
     list(f = function(...){
-          plot.inbreeding.rate(..., type = "IBS")
-         }, name = "inbreeding-rate-IBS", title = "Inbreeding rate (IBS)",
-         legend = "topleft", ylim = c(-0.1, 0.4)),
+      plot.inbreeding.rate(..., type = "IBS")
+    }, name = "inbreeding-rate-IBS", title = "Inbreeding rate (IBS)",
+    legend = "topleft", ylim = c(-0.1, 0.4)),
     list(f = function(...){
-          plot.inbreeding.rate(..., type = "IBD")
-         }, name = "inbreeding-rate-IBD", title = "Inbreeding rate (IBD)",
-         legend = "topleft", ylim = c(-0.1, 0.4)),
+      plot.inbreeding.rate(..., type = "IBD")
+    }, name = "inbreeding-rate-IBD", title = "Inbreeding rate (IBD)",
+    legend = "topleft", ylim = c(-0.1, 0.4)),
     # list(f = plot.genetic.standard.deviation, name = "genetic-sd", title = "Genetic standard deviation",
     #      legend = "topright", ylim = c(0, 0.035)),
     lost = list(f = plot.total.QTL.effect.lost, name = "QT-effect-lost", title = "Total QTL effect lost",
@@ -737,18 +737,141 @@ plot.GS.OC <- function(heritability = c(0.2, 0.5), target.dF = 0.05, file.patter
     # GS, h2 = low
     list(lty = 1, bg = "black", pch = 24),
     # OC, h2 = low
-    list(lty = 3, bg = "grey", pch = 24),
+    list(lty = 3, bg = "white", pch = 24),
     # GS, h2 = high
     list(lty = 1, bg = "black", pch = 21),
     # OC, h2 = high
-    list(lty = 3, bg = "grey", pch = 21)
+    list(lty = 3, bg = "white", pch = 21)
   )
   # set curve names
   names <- c(
     bquote(GS ~ (h^2 == .(low.h))),
-    bquote(OC ~ (h^2 == .(bquote(paste(.(low.h), ", ", sep = "") ~ C[t+1] == .(target.dF))))),
+    bquote(GOCS ~ (h^2 == .(bquote(paste(.(low.h), ", ", sep = "") ~ C[t+1] == .(target.dF))))),
     bquote(GS ~ (h^2 == .(high.h))),
-    bquote(OC ~ (h^2 == .(bquote(paste(.(high.h), ", ", sep = "") ~ C[t+1] == .(target.dF)))))
+    bquote(GOCS ~ (h^2 == .(bquote(paste(.(high.h), ", ", sep = "") ~ C[t+1] == .(target.dF)))))
+  )
+  names <- sapply(names, as.expression)
+  
+  message("Create plots ...")
+  
+  # setup plot functions
+  plot.functions <- get.plot.functions()
+  
+  # create plots
+  for(plot.fun in plot.functions){
+    
+    file <- sprintf("%s/%s.pdf", fig.dir, plot.fun$name)
+    
+    # combine small/large TP plots
+    # create.pdf(file, function(){
+    #   par(mfrow = c(1,2))
+    #   for(res in results){
+    #     plot.multi(res$data, plot.fun$f, params, ylim = plot.fun$ylim, xlim = xlim, ci = ci)
+    #     title(bquote(.(plot.fun$title) ~ .(res$title.suffix)))
+    #     add.legend(names, params, pos = plot.fun$legend)
+    #   }
+    # }, height = 5.5)
+    
+    # separate plots for small and large TP
+    
+    # add target delta F line in inbreeding plots
+    if(grepl("^inbreeding-rate", plot.fun$name)){
+      f <- plot.fun$f
+      g <- function(...){
+        f(..., deltaF.line = target.dF)
+      }
+      plot.fun$f <- g
+    }
+    
+    create.pdf(sprintf("%s/%s-TP200.pdf", fig.dir, plot.fun$name), function(){
+      res <- results[[1]]
+      plot.multi(res$data, plot.fun$f, params, ylim = plot.fun$ylim, xlim = xlim, ci = ci)
+      title(bquote(.(plot.fun$title) ~ .(res$title.suffix)))
+      add.legend(names, params, pos = plot.fun$legend)
+    }, height = 5.5, width = 7)
+    
+    create.pdf(sprintf("%s/%s-TP1000.pdf", fig.dir, plot.fun$name), function(){
+      res <- results[[2]]
+      plot.multi(res$data, plot.fun$f, params, ylim = plot.fun$ylim, xlim = xlim, ci = ci)
+      title(bquote(.(plot.fun$title) ~ .(res$title.suffix)))
+      add.legend(names, params, pos = plot.fun$legend)
+    }, height = 5.5, width = 7)
+    
+    
+  }
+  
+}
+
+# stores PDF plots in "figures/simulation/OC-RS",
+# within a subfolder according to the two included heritabilities
+plot.OC.RS <- function(heritability = c(0.2, 0.5), target.dF = 0.05, file.pattern = "bp-*.RDS", xlim = c(0,30), ci = NA){
+  
+  # check: two heritabilities
+  if(length(heritability) != 2){
+    stop("'heritability' should be a vector of length 2")
+  }
+  # extract low and high heritabilities
+  low.h <- min(heritability)
+  high.h <- max(heritability)
+  
+  fig.dir <- sprintf("figures/simulation/OC-RS/h2-%.1f-%.1f", low.h, high.h)
+  
+  # create output directory
+  if(!dir.exists(fig.dir)){
+    message(sprintf("Create output directory \"%s\"", fig.dir))
+    dir.create(fig.dir, recursive = T)
+  }
+  
+  message("Load data ...")
+  
+  # load data
+  dirs.low.h2 <- c(
+    sort(Sys.glob(sprintf("out/OC2a/30-seasons/h2-%.1f/addTP-*/normal-effects/BRR/dF-%.5f", low.h, target.dF))),
+    sort(Sys.glob(sprintf("out/RS/30-seasons/h2-%.1f/addTP-*/normal-effects/BRR", low.h)))
+  )
+  dirs.high.h2 <- c(
+    sort(Sys.glob(sprintf("out/OC2a/30-seasons/h2-%.1f/addTP-*/normal-effects/BRR/dF-%.5f", high.h, target.dF))),
+    sort(Sys.glob(sprintf("out/GS/30-seasons/h2-%.1f/addTP-*/normal-effects/BRR", high.h)))
+  )
+  data.low.h2 <- lapply(dirs.low.h2, load.simulation.results, file.pattern)
+  data.high.h2 <- lapply(dirs.high.h2, load.simulation.results, file.pattern)
+  # combine data:
+  # -- low h2 --
+  #  1) OC, h2: low, add TP: 0
+  #  2) OC, h2: low, add TP: 800
+  #  3) RS, h2: low, add TP: 0
+  #  4) RS, h2: low, add TP: 800
+  # -- high h2 --
+  #  7) OC, h2: high, add TP: 0
+  #  8) OC, h2: high, add TP: 800
+  #  9) RS, h2: high, add TP: 0
+  # 10) RS, h2: high, add TP: 800
+  data <- c(data.low.h2, data.high.h2)
+  # group small and large TP results
+  small.TP <- data[c(1,3,5,7)]
+  large.TP <- data[c(2,4,6,8)]
+  results <- list(
+    list(data = small.TP, title.suffix = "(TP = 200)"),
+    list(data = large.TP, title.suffix = "(TP = 1000)")
+  )
+  
+  # set graphical parameters
+  params <- list(
+    # OC, h2 = low
+    list(lty = 1, bg = "black", pch = 24),
+    # RS, h2 = low
+    list(lty = 3, bg = "white", pch = 24),
+    # OC, h2 = high
+    list(lty = 1, bg = "black", pch = 21),
+    # RS, h2 = high
+    list(lty = 3, bg = "white", pch = 21)
+  )
+  # set curve names
+  names <- c(
+    bquote(GOCS ~ (h^2 == .(bquote(paste(.(low.h), ", ", sep = "") ~ C[t+1] == .(target.dF))))),
+    bquote(RS ~ (h^2 == .(low.h))),
+    bquote(GOCS ~ (h^2 == .(bquote(paste(.(high.h), ", ", sep = "") ~ C[t+1] == .(target.dF))))),
+    bquote(RS ~ (h^2 == .(high.h)))
   )
   names <- sapply(names, as.expression)
   
@@ -1420,7 +1543,7 @@ plot.GS.WGS.OC.IND <- function(heritability = c(0.2, 0.5), add.TP = c(0, 800),
                                  same.inbreeding = list(delta.F = 0.02, RA.alpha = 0.35, OC.alpha = 0.65)
                                )){
   
-
+  
   for(h in heritability){
     for(add.tp in add.TP){
       tp <- add.tp + 200
@@ -1509,10 +1632,10 @@ plot.GS.WGS.OC.IND <- function(heritability = c(0.2, 0.5), add.TP = c(0, 800),
 plot.OC.IND <- function(heritability = c(0.2, 0.5), add.TP = c(0, 800),
                         file.pattern = "bp-*.RDS", xlim = c(0,30), ci = NA,
                         scenarios = list(
-                         high.short.term.gain = list(delta.F = 0.05, RA.alpha = 0.35, OC.alpha = 0.35),
-                         max.long.term.gain = list(delta.F = 0.03, RA.alpha = 0.45, OC.alpha = 0.60),
-                         same.inbreeding = list(delta.F = 0.02, RA.alpha = 0.35, OC.alpha = 0.65)
-                       )){
+                          high.short.term.gain = list(delta.F = 0.05, RA.alpha = 0.35, OC.alpha = 0.35),
+                          max.long.term.gain = list(delta.F = 0.03, RA.alpha = 0.45, OC.alpha = 0.60),
+                          same.inbreeding = list(delta.F = 0.02, RA.alpha = 0.35, OC.alpha = 0.65)
+                        )){
   
   for(h in heritability){
     for(add.tp in add.TP){
@@ -2150,9 +2273,9 @@ plot.num.fav.QTL.lost <- function(replicates,
 
 # plot total QTL effect lost
 plot.total.QTL.effect.lost <- function(replicates,
-                                        ylab = "Total QTL effect lost",
-                                        cumulative = TRUE,
-                                        ...){
+                                       ylab = "Total QTL effect lost",
+                                       cumulative = TRUE,
+                                       ...){
   
   # set function to extract total QTL effect lost
   extract.total.QTL.effect.lost <- function(seasons){
@@ -2181,9 +2304,9 @@ plot.total.QTL.effect.lost <- function(replicates,
 
 # plot total QTL effect retained
 plot.total.QTL.effect.retained <- function(replicates,
-                                       ylab = "Total QTL effect retained",
-                                       cumulative = TRUE,
-                                       ...){
+                                           ylab = "Total QTL effect retained",
+                                           cumulative = TRUE,
+                                           ...){
   
   # set function to extract total QTL effect retained
   extract.total.QTL.effect.retained <- function(seasons){
